8c7e6ada9bc287a1470bdda5b434b1443a95fd02
molodoyzxc <semenov.svin@gmail.com>
2024-11-08 16:08:04 +0300

    toaster

--- scripts/constructor.js
+++ scripts/constructor.js
@@ -2,11 +2,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('tableForm');
     const tableContainer = document.getElementById('tableContainer');
     loadFormData();
+
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         const days = document.getElementById('days').value;
         const classesPerDay = document.getElementById('classesPerDay').value;
         const language = document.getElementById('language').value;
+
+        if (classesPerDay > 10) {
+            Toastify({
+                text: language === 'ru' ? 'Ошибка: Уроков не может быть больше 10' : 'Error: Classes per day must not exceed 10',
+                duration: 3000,
+                close: true,
+                gravity: "bottom",
+                position: "right",
+                backgroundColor: "red",
+            }).showToast();
+            return;
+        }
+
         saveFormData(days, classesPerDay, language);
         generateTable(days, classesPerDay, language);
     });
@@ -23,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function loadFormData() {
         const savedData = localStorage.getItem('tableFormData');
         if (savedData) {
-            const {days, classesPerDay, language} = JSON.parse(savedData);
+            const { days, classesPerDay, language } = JSON.parse(savedData);
             document.getElementById('days').value = days;
             document.getElementById('classesPerDay').value = classesPerDay;
             document.getElementById('language').value = language;
@@ -35,6 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const table = document.createElement('table');
         table.classList.add('generated-table');
         const headerRow = document.createElement('tr');
+
         for (let i = 0; i <= classesPerDay; i++) {
             const headerCell = document.createElement('th');
             if (i === 0) {
@@ -45,6 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
             headerRow.appendChild(headerCell);
         }
         table.appendChild(headerRow);
+
         for (let day = 1; day <= days; day++) {
             const row = document.createElement('tr');
             const dayCell = document.createElement('td');
