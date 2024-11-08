181418bd0b23458ee5c5afb7d7adeb0dcee317e1
molodoyzxc <semenov.svin@gmail.com>
2024-11-08 15:10:09 +0300

    slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new Swiper('.swiper-container', {
+        loop: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        effect: 'coverflow',
+        coverflowEffect: {
+            rotate: 30,
+            stretch: 150,
+            depth: 200,
+            modifier: 1,
+            slideShadows: false
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: true
+        }
+    });
+});
+
+
+
+
 (function() {
     window.addEventListener('load', function() {
         const loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
